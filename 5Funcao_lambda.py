168a9af14aa2b6790d0bf105abb76e56b39a0986
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-04-12 21:02:30 -0300

    Criando funções lambdas

---
+++ 5Funcao_lambda.py
@@ -0,0 +1,14 @@
+# uma função lambda se escreve da seguinte forma: lambda argumento: exprexão
+
+# Uma função que tem o unico objetivo que é retornar algo, pode ser escrita na forma de
+# função lambda
+
+x = lambda a: a + 10
+
+print(x(5))
+
+# Passando dois argumentos
+
+aluno = lambda nome, sobre_nome: print("Dados do aluno: " + nome, sobre_nome)
+
+aluno("Felipe", "Oliveira")
